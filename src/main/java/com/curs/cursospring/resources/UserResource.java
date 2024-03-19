a99a4bd913b1526add65b67f099ae20cf4e64ba3
Aaron <aarongibran13@gmail.com>
2024-03-19 10:29:37 -0300

    User entity and resource

---
+++ src/main/java/com/curs/cursospring/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.curs.cursospring.resources;
+
+import com.curs.cursospring.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u  = new User(1L,"Aaron","agm@gmail.com", "9999999", "123444465");
+        return ResponseEntity.ok().body(u);
+    }
+
+
+}
