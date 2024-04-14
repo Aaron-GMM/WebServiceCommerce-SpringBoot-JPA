cc4ec37bd0ec0840c5c5ae69edb3308dfd379681
Aaron <aarongibran13@gmail.com>
2024-04-14 15:47:39 -0300

    Subtotal total methods

--- src/main/java/com/curs/cursospring/entities/Order.java
+++ src/main/java/com/curs/cursospring/entities/Order.java
@@ -83,6 +83,14 @@ public class Order implements Serializable {
         return items;
     }
 
+
+    public Double getTotal(){
+        double sum  = 0.00;
+        for (OrderItem x: items){
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
