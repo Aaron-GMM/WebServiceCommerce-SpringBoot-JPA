2fe54032aebbeef5b705d835b03e55d2f4d46d1f
Aaron <aarongibran13@gmail.com>
2024-04-14 20:20:51 -0300

    User delete

--- src/main/java/com/curs/cursospring/resources/UserResource.java
+++ src/main/java/com/curs/cursospring/resources/UserResource.java
@@ -36,5 +36,12 @@ public class UserResource {
 
     }
 
+    @DeleteMapping(value = "/{id}")
+    public  ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 }
